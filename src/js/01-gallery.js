2482ebaab242895954a58b7eaf792ebf43e6c030
ruskava <94966888+ruskava@users.noreply.github.com>
2022-09-02 16:19:17 +0300

    1

---
+++ src/js/01-gallery.js
@@ -0,0 +1,8 @@
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
+
+
+console.log(galleryItems);
+
+import SimpleLightbox from 'simpleLightbox/dist/simple-lightbox.esm'
