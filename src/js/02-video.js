d8d7a2116cc4385bac99088c6f6172e92eb277e2
ruskava <94966888+ruskava@users.noreply.github.com>
2022-09-06 18:10:58 +0300

    допрацювання коду

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -7,7 +7,7 @@ const savedTime =localStorage.getItem('videoplayer-current-time') || 0;
 console.log(savedTime);
 player.on('timeupdate', throttle(onPlay, 1000));
 function onPlay(data) {
-    localStorage.setItem ('videoplayer-current-time', data.seconds)
+localStorage.setItem ('videoplayer-current-time', data.seconds)
 };
 player.setCurrentTime(savedTime);
   
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,31 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const email = document.querySelector('input');
+const message = document.querySelector('textarea');
+const STORAGE_KEY = 'feedback-form-state';
+
+form.addEventListener('input', throttle(textAreaInput, 500));
+form.addEventListener('submit', onFormSubmit);
+
+function textAreaInput(e) {
+    const dataStorage = {
+      email: email.value,
+      message: message.value,
+    }
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(dataStorage));
+  };
+  
+  function onFormSubmit(e) {
+    e.preventDefault();
+    console.log(localStorage.getItem(STORAGE_KEY));
+    localStorage.removeItem(STORAGE_KEY);
+    form.reset();
+  }
+  savedTextArea();
+  
+  function savedTextArea() {
+    const savedMessage = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
+    email.value = savedMessage.email || '';
+    message.value = savedMessage.message || '';
+  };
